36c2aee0625be25b58b0ab130d4c61249ff1d949
Levi <291524262@qq.com>
2020-10-19 11:12:43 +0800

    swagger2 update
    swagger2 接口文档加入

--- service/service-edu/src/main/java/com/atguigu/edu/controller/TeacherController.java
+++ service/service-edu/src/main/java/com/atguigu/edu/controller/TeacherController.java
@@ -3,6 +3,9 @@ package com.atguigu.edu.controller;
 
 import com.atguigu.edu.entity.Teacher;
 import com.atguigu.edu.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,7 @@ import java.util.List;
  * @author Levi
  * @since 2020-10-14
  */
+@Api(consumes = "讲师管理")
 @RestController
 @RequestMapping("/edu/teacher")
 @CrossOrigin //跨域
@@ -29,14 +33,16 @@ public class TeacherController {
     }
 
     //1.查询所有讲师数据 http://localhost:8001/edu/teacher/findAll
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
     public List<Teacher> teacherList() {
         return teacherService.list(null);
     }
 
     //讲师逻辑删除功能
+    @ApiOperation(value = "根据ID逻辑删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeById(@PathVariable String id) {
+    public boolean removeById(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
         return teacherService.removeById(id);
     }
 }
